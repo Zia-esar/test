3bba243d7e920e0f62d02f8b91f3c143791feb1f
zia-esar <ziaesar@gmail.com>
2018-03-13 21:51:01 +0500

    Added new console app

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.Write("Enter your Marks= ");
+             short yourmarks = Convert.ToInt16(Console.ReadLine());
+             Console.Write("Enter total Marks= ");
+             short totalmarks = Convert.ToInt16(Console.ReadLine());
+             int percent = (yourmarks / totalmarks) * 100;
+             Console.Write("Your percentage is = " + percent);
+             */
+            
+            Console.ReadKey();
+        }
+    }
+}
